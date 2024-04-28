55db6f8d859d3dcd205be65637d69693753d8aa4
Sha-hed <shahedcse14@gmail.com>
2024-04-29 02:24:44 +0600

    Get By Id method added

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ async function run() {
       res.send(result);
     });
 
-    // app.get("/spots/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await TouristDestination.findOne(query);
-    //   res.send(result);
-    // });
+    app.get("/spots/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await TouristDestination.findOne(query);
+      res.send(result);
+    });
     // app.get("/mySpots/:email", async (req, res) => {
     //   const email = req.params.email;
     //   const query = { email: email };
